abad02c55f2cc11db279fda4836fc04067dbdc5b
LeeMatsuda <48104119+LeeMatsuda@users.noreply.github.com>
2019-05-31 18:46:28 +0400

    Create Rotor.cs

---
+++ Rotor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotor : MonoBehaviour {
+	public Vector3 rotationDirection = new Vector3 (0f,0f,1f);
+	public KeyCode rotationKey = KeyCode.E;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.E)) {
+			transform.Rotate (rotationDirection);
+		} else {
+			transform.Rotate (-rotationDirection);
+		}
+		}
+		
+}
